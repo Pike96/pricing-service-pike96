cd2f85e388ee9e88eb84fc13e88593798a481c1d
pike <pike@macbookPike.local>
2017-11-07 05:27:30 -0800

    Add all (1st commit)

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,11 @@
+import os
+
+COLLECTION = 'alerts'
+
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
+
+ALERT_TIMEOUT = 10
+
+COLLECTION = "alerts"
